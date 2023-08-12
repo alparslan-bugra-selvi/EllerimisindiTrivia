5ed913ac2145e79321897126cbe91961fb0d0fc1
Alparslan Buğra Selvi <alparslan.bugra@hotmail.com>
2023-08-12 15:35:28 +0300

    Initial Commit

---
+++ EllerimisindiTrivia/EllerimisindiTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EllerimisindiTriviaApp.swift
+//  EllerimisindiTrivia
+//
+//  Created by Alp Selvi on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct EllerimisindiTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
